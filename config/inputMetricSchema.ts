75cd5529291c663a3f117f961bfee5fc9cc8200d
PRASUN-SITAULA <psitaula209@gmail.com>
2024-10-10 21:22:06 +0545

    form error fixed

--- config/inputMetricSchema.ts
+++ config/inputMetricSchema.ts
@@ -1,16 +1,22 @@
 import { z } from "zod"
 
-export const inputMetricSchema = z.object({
+export const heightSchema = z.object({
   height: z
     .number({
       invalid_type_error: "Height must be a positive number.",
     })
     .nonnegative(),
+})
+
+export const weightSchema = z.object({
   weight: z
     .number({
       invalid_type_error: "Weight must be a positive number.",
     })
     .nonnegative(),
+})
+
+export const bloodGlucoseSchema = z.object({
   bloodGlucose: z
     .number({
       invalid_type_error: "Blood Glucose must be a positive number.",
